8e7e8bae2bde466fc7fba16de2609c14a8c4bf7d
JUAN ALVARADO <juanalvaradocor01@gmail.com>
2025-05-12 19:14:07 -0500

    Descripción de los cambios realizados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'inicio',
+    loadChildren: () => import('./pages/inicio/inicio.module').then(m => m.InicioPageModule)
+  },
+  {
+    path: 'datetime',
+    loadChildren: () => import('./pages/datetime/datetime.module').then(m => m.DatetimePageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'inicio',
     pathMatch: 'full'
   },
+  {
+    path: 'datetime',
+    loadChildren: () => import('./pages/datetime/datetime.module').then( m => m.DatetimePageModule)
+  }
 ];
 
 @NgModule({
---
+++ src/app/pages/datetime/datetime.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-datetime',
+  templateUrl: './datetime.page.html',
+  styleUrls: ['./datetime.page.scss'],
+})
+export class DatetimePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
